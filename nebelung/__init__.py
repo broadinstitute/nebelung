f5c68302c71b6274f4c44bf20096501c15c34c28
Devin McCabe <dmccabe@broadinstitute.org>
2024-08-22 09:23:30 -0400

    don't need future annot's

--- nebelung/__init__.py
+++ nebelung/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from importlib import metadata as importlib_metadata
 
 
--- nebelung/types.py
+++ nebelung/types.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from typing import Any, NotRequired, Optional, TypedDict, TypeVar
 
 import pandera as pa
